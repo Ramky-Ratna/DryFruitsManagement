535899a3019c249d809c23f6fe7eec703b62d248
K Ramakrishna <neelu.6719@outlook.com>
2020-09-18 15:01:34 +0530

    Added SqlQuery File

---
+++ ProductsSQLQuery.sql
@@ -0,0 +1,30 @@
+﻿create table ProductType(
+	TypeId int identity(1,1) primary key,
+	ProductType varchar(100),
+	AddedDate datetime,
+	Modified datetime
+)
+
+create table TblProducts(
+	ProductId int identity(1,1) primary key,
+	ProductType int,
+	ProductName varchar(100),
+	Description varchar(max),
+	Price decimal,
+	Quntity int,
+	AddedDate datetime,
+	Modified datetime,
+	FOREIGN KEY (ProductType) references ProductType(TypeId)
+)
+
+select * from ProductType
+
+insert into ProductType(ProductType,AddedDate) values('Anjeer',getdate())
+insert into ProductType(ProductType,AddedDate) values('Almond',getdate())
+insert into ProductType(ProductType,AddedDate) values('Apricot',getdate())
+insert into ProductType(ProductType,AddedDate) values('Cashew',getdate())
+insert into ProductType(ProductType,AddedDate) values('Dates',getdate())
+insert into ProductType(ProductType,AddedDate) values('Kismis',getdate())
+insert into ProductType(ProductType,AddedDate) values('Kiwi',getdate())
+insert into ProductType(ProductType,AddedDate) values('Pista',getdate())
+insert into ProductType(ProductType,AddedDate) values('Walnuts',getdate())
--- SQLQuery1.sql
+++
@@ -1,8 +0,0 @@
-﻿create table TblProducts(
-	ProductId int identity(1,1) primary key,
-	ProductName varchar(100),
-	Price decimal,
-	Quantity int,
-	AddedDate datetime,
-	Modified datetime
-)
